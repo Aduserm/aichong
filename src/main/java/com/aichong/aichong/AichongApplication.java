f124c12410a8dedd1310584a767bc83f0ec8465e
Linjingji <1345830103@qq.com>
2019-09-10 12:04:07 +0800

    Initial commit

---
+++ src/main/java/com/aichong/aichong/AichongApplication.java
@@ -0,0 +1,18 @@
+package com.aichong.aichong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author USER
+ * @Description //TODO
+ * @Date 21:03 2019/9/9
+ **/
+@SpringBootApplication
+public class AichongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AichongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aichong/aichong/AichongApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aichong.aichong;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AichongApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
